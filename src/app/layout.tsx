23ea32c4c6cbc1d2f46c28961b6333b6f166ab4b
Sam Joe Chalissery <samjoe55555@gmail.com>
2025-07-15 13:01:50 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/next"
 import ClientShell from '@/components/ClientShell'; // client-only logic
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-black text-black dark:text-white transition-colors duration-200`}
       >
         <ClientShell>{children}</ClientShell>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
